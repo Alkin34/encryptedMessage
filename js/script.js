4471125ff7bb4c534b98dcddd5bd77ed97b56b4c
Alkin34 <alkatras.334@yandex.ru>
2022-10-04 12:50:16 +0300

    Написал функция шифрования слова.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+let container = document.querySelector(".container");
 let inputMsg = document.querySelector("#msg");
+let inputKey = document.querySelector("#key");
 let button = document.querySelector(".btn");
 
 let arr_ru = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'];
 let arr_RU = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ', 'Э', 'Ю', 'Я'];
+
+container = addEventListener('click', encrypt);
+
+function encrypt(e) {
+    if (e.target === button) {
+        let inputMsgValue = inputMsg.value;
+        let inputKeyValue = Number(inputKey.value);
+        console.log(inputMsgValue);
+        console.log(inputKeyValue);
+        let encryptedArr = [];
+        for (let i = 0; i < inputMsgValue.length; i++) {
+            let symbolMsg = inputMsgValue[i];
+            let indexSimbolArr_ru = arr_ru.indexOf(symbolMsg);
+            let newIndexSimbol = indexSimbolArr_ru + inputKeyValue;
+            let newSimbolArr = arr_ru[newIndexSimbol]
+            encryptedArr.push(newSimbolArr);
+        }
+        let encryptedMsg = encryptedArr.join('');
+        console.log(encryptedMsg);
+    }
+
+
+}
